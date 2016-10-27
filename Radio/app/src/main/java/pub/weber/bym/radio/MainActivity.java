ff2821bc077928eb179bed01911cf99393c6b2a6
baiyuming <bai.yu.ming@163.com>
2016-10-27 20:11:05 +0800

    Radio  单选按钮  事件

--- Radio/app/src/main/java/pub/weber/bym/radio/MainActivity.java
+++ Radio/app/src/main/java/pub/weber/bym/radio/MainActivity.java
@@ -2,12 +2,38 @@ package pub.weber.bym.radio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RadioGroup radioGroup;
+    private RadioButton maleButton;
+    private RadioButton femaleButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        radioGroup = (RadioGroup) findViewById(R.id.radioId);
+        maleButton = (RadioButton) findViewById(R.id.maleId);
+        femaleButton = (RadioButton) findViewById(R.id.femaleId);
+
+        RadioGroupListener radioGroupListener = new RadioGroupListener();
+        radioGroup.setOnCheckedChangeListener(radioGroupListener);
+
+    }
+
+    class RadioGroupListener implements RadioGroup.OnCheckedChangeListener{
+        @Override
+        public void onCheckedChanged(RadioGroup group,int checkedId){
+            if (checkedId == maleButton.getId()){
+                System.out.println("====男人=====");
+            }else if (checkedId == femaleButton.getId()){
+                System.out.println("****女人*****");
+            }
+        }
     }
 }
