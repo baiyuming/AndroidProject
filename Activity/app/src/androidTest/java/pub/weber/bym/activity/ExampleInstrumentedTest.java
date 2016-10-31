0e716ca20266bb7e49c02ce30cb963723e1787dd
baiyuming <bai.yu.ming@163.com>
2016-10-31 16:06:56 +0800

    Activity Test Demo

--- Activity/app/src/androidTest/java/pub/weber/bym/activity/ExampleInstrumentedTest.java
+++ Activity/app/src/androidTest/java/pub/weber/bym/activity/ExampleInstrumentedTest.java
@@ -18,7 +18,7 @@ import static org.junit.Assert.*;
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() throws Exception {
-        // Context of the app under test.
+        // Context of the activity_auth under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("pub.weber.bym.activity", appContext.getPackageName());
--- Activity/app/src/main/java/pub/weber/bym/activity/AppActivity.java
+++ Activity/app/src/main/java/pub/weber/bym/activity/AppActivity.java
@@ -8,6 +8,6 @@ public class AppActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.app);
+        setContentView(R.layout.activity_auth);
     }
 }
