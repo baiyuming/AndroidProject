10741b446e6ad906c65d4ebed3548da1d5c5c61a
baiyuming <bai.yu.ming@163.com>
2016-11-02 14:00:27 +0800

    安卓编辑框

---
+++ Activity/app/src/main/java/pub/weber/bym/activity/EditTextTest.java
@@ -0,0 +1,13 @@
+package pub.weber.bym.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditTextTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_text_test);
+    }
+}
