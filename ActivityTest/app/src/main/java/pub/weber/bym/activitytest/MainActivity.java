406297b795ebc7c1ad1b8886c7a0e77f20d839bc
baiyuming <bai.yu.ming@163.com>
2016-11-04 14:09:12 +0800

    活动

--- ActivityTest/app/src/main/java/pub/weber/bym/activitytest/MainActivity.java
+++ ActivityTest/app/src/main/java/pub/weber/bym/activitytest/MainActivity.java
@@ -1,13 +1,51 @@
 package pub.weber.bym.activitytest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//隐藏标题栏
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button_1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this,"你点了按钮",Toast.LENGTH_SHORT).show();
+                //finish();// 销毁活动
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.add_item:
+                Toast.makeText(this,"你点击了添加",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this,"你点击了删除",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return true;
     }
 }
