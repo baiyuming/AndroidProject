97160e392f7395099ee79a6a075827df89c6ff2d
baiyuming <bai.yu.ming@163.com>
2016-11-15 17:33:10 +0800

    聊天界面

---
+++ UIBestPractice/app/src/main/java/pub/weber/bym/uibestpractice/Msg.java
@@ -0,0 +1,22 @@
+package pub.weber.bym.uibestpractice;
+
+/**
+ * Created by BYM on 2016/11/15.
+ */
+
+public class Msg {
+    public static final int TYPE_RECEIVED = 0;
+    public static final int TYPE_SENT = 1;
+    private String content;
+    private int type;
+    public Msg(String content,int type){
+        this.content = content;
+        this.type = type;
+    }
+    public String getContent(){
+        return content;
+    }
+    public int getType(){
+        return type;
+    }
+}
