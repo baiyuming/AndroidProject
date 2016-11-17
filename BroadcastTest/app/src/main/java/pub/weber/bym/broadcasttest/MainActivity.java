790ca5e9a4256b2d495255e3886b93addfaa19bf
baiyuming <bai.yu.ming@163.com>
2016-11-17 11:33:27 +0800

    发送标准广播

--- BroadcastTest/app/src/main/java/pub/weber/bym/broadcasttest/MainActivity.java
+++ BroadcastTest/app/src/main/java/pub/weber/bym/broadcasttest/MainActivity.java
@@ -8,6 +8,8 @@ import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -19,10 +21,18 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        intentFilter = new IntentFilter();
-        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
-        networkChangeReceiver = new NetworkChangeReceiver();
-        registerReceiver(networkChangeReceiver,intentFilter);
+//        intentFilter = new IntentFilter();
+//        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+//        networkChangeReceiver = new NetworkChangeReceiver();
+//        registerReceiver(networkChangeReceiver,intentFilter);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("com.example.broadcasttest.MY_BROADCAST");
+                sendBroadcast(intent);
+            }
+        });
     }
 
     @Override
---
+++ BroadcastTest/app/src/main/java/pub/weber/bym/broadcasttest/MyBroadcastReceiver.java
@@ -0,0 +1,18 @@
+package pub.weber.bym.broadcasttest;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
+
+/**
+ * Created by BYM on 2016/11/17.
+ */
+
+public class MyBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent){
+        Toast.makeText(context,"received in MyBroadcastReceiver",Toast.LENGTH_SHORT).show();
+    }
+}
