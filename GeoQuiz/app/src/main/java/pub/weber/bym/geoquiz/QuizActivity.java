d9b7c3732182c8b4110b80bab6107924ab339039
baiyuming <bai.yu.ming@163.com>
2016-11-24 15:48:49 +0800

    知识问答 启动另一个activity

--- GeoQuiz/app/src/main/java/pub/weber/bym/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/pub/weber/bym/geoquiz/QuizActivity.java
@@ -1,13 +1,19 @@
 package pub.weber.bym.geoquiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
+    private Button mCheatButton;
 
     private Button mTrueButton;
     private Button mFalseButton;
@@ -31,9 +37,24 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Log.d(TAG, "onCreate(Bundle) 被调用");
+
         setContentView(R.layout.activity_quiz);
 
+        mCheatButton = (Button) findViewById(R.id.cheat_button);
+        mCheatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(QuizActivity.this,CheatActivity.class);
+                startActivity(intent);
+            }
+        });
+
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
         updateQuestion();
 
         mTrueButton = (Button) findViewById(R.id.true_button);
@@ -100,4 +121,37 @@ public class QuizActivity extends AppCompatActivity {
 
         Toast.makeText(this,messageResId,Toast.LENGTH_SHORT).show();
     }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(TAG,"onStart() 被调用");
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() 被调用");
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() 被调用");
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() 被调用");
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() 被调用");
+    }
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
 }
