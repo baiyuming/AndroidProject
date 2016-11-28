1bc361c32ab249c282594c94003e96784ad30ab8
baiyuming <bai.yu.ming@163.com>
2016-11-28 16:33:13 +0800

    网络项目

--- WebViewTest/app/src/main/java/pub/weber/bym/webviewtest/MainActivity.java
+++ WebViewTest/app/src/main/java/pub/weber/bym/webviewtest/MainActivity.java
@@ -1,13 +1,29 @@
 package pub.weber.bym.webviewtest;
 
+import android.support.v4.media.MediaMetadataCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.web_view);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view,String url){
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        mWebView.loadUrl("http://weber.pub");
     }
 }
